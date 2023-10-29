cab378b20ed951f945ce89eed3741e3125b69678
Soma Molnar <soma.matyas.molnar@gmail.com>
2023-10-29 09:45:28 +0100

    Endpoints added to the backend

--- Code/backend/facial_keypoint.py
+++ Code/backend/facial_keypoint.py
@@ -1,33 +1,68 @@
 from pydantic import BaseModel
 
+
+class Position(BaseModel):
+    x_Coordinate: float
+    y_Coordinate: float
+
 class FacialKeyPoints(BaseModel):
-    left_eye_center_x: float	
-    left_eye_center_y: float
-    right_eye_center_x: float	
-    right_eye_center_y: float	
-    left_eye_inner_corner_x: float
-    left_eye_inner_corner_y: float
-    left_eye_outer_corner_x: float
-    left_eye_outer_corner_y: float
-    right_eye_inner_corner_x: float
-    right_eye_inner_corner_y: float
-    right_eye_outer_corner_x: float
-    right_eye_outer_corner_y: float
-    left_eyebrow_inner_end_x: float
-    left_eyebrow_inner_end_y: float
-    left_eyebrow_outer_end_x: float
-    left_eyebrow_outer_end_y: float
-    right_eyebrow_inner_end_x: float
-    right_eyebrow_inner_end_y: float
-    right_eyebrow_outer_end_x: float
-    right_eyebrow_outer_end_y: float
-    nose_tip_x: float
-    nose_tip_y: float
-    mouth_left_corner_x: float	
-    mouth_left_corner_y: float
-    mouth_right_corner_x: float
-    mouth_right_corner_y: float
-    mouth_center_top_lip_x: float
-    mouth_center_top_lip_y: float
-    mouth_center_bottom_lip_x: float
-    mouth_center_bottom_lip_y: float
+    # left_eye_center_x: float	
+    # left_eye_center_y: float
+    left_eye_center: Position | None = None
+
+    # right_eye_center_x: float	
+    # right_eye_center_y: float
+    right_eye_center: Position | None = None
+
+    # left_eye_inner_corner_x: float
+    # left_eye_inner_corner_y: float
+    left_eye_inner_corner: Position | None = None
+
+    # left_eye_outer_corner_x: float
+    # left_eye_outer_corner_y: float
+    left_eye_outer_corner: Position | None = None
+
+    # right_eye_inner_corner_x: float
+    # right_eye_inner_corner_y: float
+    right_eye_inner_corner: Position | None = None
+
+    # right_eye_outer_corner_x: float
+    # right_eye_outer_corner_y: float
+    right_eye_outer_corner: Position | None = None
+ 
+    # left_eyebrow_inner_end_x: float
+    # left_eyebrow_inner_end_y: float
+    left_eyebrow_inner_end: Position | None = None
+
+    # left_eyebrow_outer_end_x: float
+    # left_eyebrow_outer_end_y: float
+    left_eyebrow_outer_end: Position | None = None
+
+    # right_eyebrow_inner_end_x: float
+    # right_eyebrow_inner_end_y: float
+    right_eyebrow_inner_end: Position | None = None
+
+    # right_eyebrow_outer_end_x: float
+    # right_eyebrow_outer_end_y: float
+    right_eyebrow_outer_end: Position | None = None
+
+    # nose_tip_x: float
+    # nose_tip_y: float
+    nose_tip: Position | None = None
+
+    # mouth_left_corner_x: float	
+    # mouth_left_corner_y: float
+    mouth_left_corner: Position | None = None
+
+    # mouth_right_corner_x: float
+    # mouth_right_corner_y: float
+    mouth_right_corner: Position | None = None
+
+    # mouth_center_top_lip_x: float
+    # mouth_center_top_lip_y: float
+    mouth_center_top_lip: Position | None = None
+
+    # mouth_center_bottom_lip_x: float
+    # mouth_center_bottom_lip_y: float
+    mouth_center_bottom_lip: Position | None = None
+
--- Code/backend/main.py
+++ Code/backend/main.py
@@ -4,7 +4,10 @@ from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+from facial_keypoint import FacialKeyPoints, Position
+
 import tensorflow as tf
+import numpy as np
 
 class Picture(BaseModel):
     pixels: list = []
@@ -16,21 +19,67 @@ ml_model_50_epochs = tf.keras.models.load_model('./Models/50/facial_keypoint_mod
 ml_model_100_epochs = tf.keras.models.load_model('./Models/100/facial_keypoint_model_100_epochs.keras')
 
 
-
 app = FastAPI()
 
 @app.get("/facialkeypoints/epochs_10")
-async def get_facial_keypoints(picture: Picture):
-    print(picture.pixels)
-    return {"Epochs": "10"}
+async def get_facial_keypoints(picture: Picture) -> FacialKeyPoints:
+    formatedPixels = preparePixelData(picture.pixels)
+    predicted_points = ml_model_10_epochs.predict(formatedPixels)[0]
+    facialKeypoints = convertToFacialKeypoints(predicted_points)
+    return facialKeypoints
 
 @app.get("/facialkeypoints/epochs_50")
 async def get_facial_keypoints(picture: Picture):
-    print(picture.pixels)
-    return {"Epochs": "50"}
+    formatedPixels = preparePixelData(picture.pixels)
+    predicted_points = ml_model_50_epochs.predict(formatedPixels)[0]
+    facialKeypoints = convertToFacialKeypoints(predicted_points)
+    return facialKeypoints
 
 @app.get("/facialkeypoints/epochs_100")
 async def get_facial_keypoints(picture: Picture):
-    print(picture.pixels)
-    return {"Epochs": "100"}
+    formatedPixels = preparePixelData(picture.pixels)
+    predicted_points = ml_model_100_epochs.predict(formatedPixels)[0]
+    facialKeypoints = convertToFacialKeypoints(predicted_points)
+    return facialKeypoints
+
+
+
+def preparePixelData(pixels):
+    data = []
+    test_imgs_arr = np.array(pixels, dtype='float')
+    test_imgs_arr = np.reshape(test_imgs_arr, (1, 96, 96, 1))
+    data = test_imgs_arr/255.
+    return data
+
+def convertToFacialKeypoints(predicted_points):
+    facialKeyPoints = FacialKeyPoints()
+
+    positions = []
+    for i in range(len(predicted_points)//2):
+       
+        x = predicted_points[i]
+        y = predicted_points[i + 1]
+        position = Position(x_Coordinate = x, y_Coordinate = y)
+        positions.append(position)
+        i += 2
+    print(positions)
+    facialKeyPoints.left_eye_center = positions[0]
+    facialKeyPoints.right_eye_center = positions[1]
+    facialKeyPoints.left_eye_inner_corner = positions[2]
+    facialKeyPoints.left_eye_outer_corner = positions[3]
+    facialKeyPoints.right_eye_inner_corner = positions[4]
+    facialKeyPoints.right_eye_outer_corner = positions[5]
+    facialKeyPoints.left_eyebrow_inner_end = positions[6]
+    facialKeyPoints.left_eyebrow_outer_end = positions[7]
+    facialKeyPoints.right_eyebrow_inner_end = positions[8]
+    facialKeyPoints.right_eyebrow_outer_end = positions[9]
+    facialKeyPoints.nose_tip = positions[10]
+    facialKeyPoints.mouth_left_corner = positions[11]
+    facialKeyPoints.mouth_right_corner = positions[12]
+    facialKeyPoints.mouth_center_top_lip = positions[13]
+    facialKeyPoints.mouth_center_bottom_lip = positions[14]
+
+    print(facialKeyPoints)
+
+    return facialKeyPoints
 
